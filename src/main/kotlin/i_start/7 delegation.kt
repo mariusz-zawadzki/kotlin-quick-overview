d232fa992fbdbcaddcd9ab17168a833f88983bc9
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-06-19 21:11:07 +0200

    short on delegates

---
+++ src/main/kotlin/i_start/7 delegation.kt
@@ -0,0 +1,44 @@
+package i_start
+
+
+//wrapper by delegations
+class SomeClass<T>(val delegate: List<T>) : List<T> by delegate {
+
+    fun <R> map(transform: (T) -> R): SomeClass<R> {
+        return SomeClass(delegate.map(transform))
+    }
+    // it can be either this up or as an extension function
+
+
+    //we need toString because it is not a dataclass (it can be though)
+    override fun toString(): String {
+        return delegate.toString()
+    }
+}
+
+
+//property delegation
+class LazyPropertyUsingDelegates(initializer: () -> Int) {
+    val lazyValue: Int by lazy(initializer)
+}
+
+fun main(args: Array<String>) {
+    val someList: SomeClass<String> = SomeClass(listOf("apples", "bananas"))
+
+    println(someList)
+
+    val epplesAndBenenies: SomeClass<String> = someList.map { it.replace("a", "e") }
+
+    println(epplesAndBenenies)
+
+    val lazyPropertyUsingDelegates = LazyPropertyUsingDelegates {
+        println("sideffect happens once")
+        12
+    }
+    println("Before initalization")
+    println(lazyPropertyUsingDelegates.lazyValue)
+    println(lazyPropertyUsingDelegates.lazyValue)
+}
+
+
+
