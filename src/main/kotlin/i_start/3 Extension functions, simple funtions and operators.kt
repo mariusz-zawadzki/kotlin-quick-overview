e58034fc6930f9c7d56252639767fe679038f4ce
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-06-19 17:45:52 +0200

    extension functions and operators

---
+++ src/main/kotlin/i_start/3 Extension functions, simple funtions and operators.kt
@@ -0,0 +1,21 @@
+package i_start
+
+
+data class Point(val x: Int, val y: Int)
+
+fun Point.metric(): Int = x * x + y * y
+
+fun Point.compareTo(p: Point): Int {
+    return this.metric().compareTo(p.metric())
+}
+
+fun String.doSomethingWithMe() {
+    println(this.toUpperCase() + this.toLowerCase())
+
+//    println(Point(2, 2) >= Point(1, 1))
+}
+
+
+fun main(args: Array<String>) {
+    "Some concrete string".doSomethingWithMe()
+}
