c65c7df57f597e679fa9b0840e132f01dec722de
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-06-19 17:46:43 +0200

    move constants

--- src/main/kotlin/i_start/1 ValVarNulls.kt
+++ src/main/kotlin/i_start/1 ValVarNulls.kt
@@ -1,9 +1,9 @@
 package i_start
 
-const val A_CONSTANT_VALUE = "A constant"
-var aNormalVariable = 42.0
 
+const val A_CONSTANT_VALUE = "A constant"
 fun main(args: Array<String>) {
+    var aNormalVariable = 42.0
     var `only local variables can be named like this` = 12
 
     println(A_CONSTANT_VALUE)
