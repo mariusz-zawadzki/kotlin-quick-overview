bd690b165f09f1faf34fbd18b8c2bfd49a50bb08
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-06-19 19:52:40 +0200

    var val fixes

--- src/main/kotlin/i_start/1 ValVarNulls.kt
+++ src/main/kotlin/i_start/1 ValVarNulls.kt
@@ -6,6 +6,13 @@ var aNormalVariable = 42.0
 
 
 fun main(args: Array<String>) {
+    var iNullable: Int?
+    var i: Int = 1
+
+    iNullable = i //not null to null
+    iNullable = null
+    //i = null // will not compile
+    i = 2 //will work fine
     var `only local variables can be named like this` = 12
 
     println(A_CONSTANT_VALUE)
